534a4c5e4460800a57dd21c1cc1b2d8a5469b90b
Al-Fhaigar J. Usman <93189177+rubickking04@users.noreply.github.com>
2023-01-13 21:14:39 +0800

    gg

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -53,7 +53,7 @@ class CartController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  int  $id~
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
