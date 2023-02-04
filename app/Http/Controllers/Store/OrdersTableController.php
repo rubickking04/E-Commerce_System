28020fb1f384465d4f9b3b6ee24cd34f140bd9db
Al-Fhaigar J. Usman <93189177+rubickking04@users.noreply.github.com>
2023-02-04 18:13:35 +0800

    Update OrdersTableController.php

--- app/Http/Controllers/Store/OrdersTableController.php
+++ app/Http/Controllers/Store/OrdersTableController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers\Store;
 
+use Carbon\Carbon;
 use App\Models\Cart;
 use App\Models\Order;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 use App\Models\DeliveryStatus;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use RealRashid\SweetAlert\Facades\Alert;
 use Illuminate\Contracts\Database\Query\Builder;
@@ -26,9 +27,9 @@ class OrdersTableController extends Controller
         $product_sold= Order::where('store_id',Auth::id())->sum('qty');
         $total_sales = Order::where('store_id', Auth::id())->sum('total_price');
         $yearly_sales = Order::whereYear('created_at', $year)->whereMonth('created_at', $month)->sum('total_price');
-        $cart = Order::with('hasStatus')->where('store_id', '=', Auth::user()->id)->latest()->paginate(10);
-        // dd($cart);
-        return view('store.orders_table', compact('cart', 'total_orders', 'product_sold', 'total_sales', 'yearly_sales'));
+        $cart = Order::where('store_id', '=', Auth::user()->id)->orderBy('created_at', 'DESC')->latest()->paginate(10);
+        dd($cart);
+        // return view('store.orders_table', compact('cart', 'total_orders', 'product_sold', 'total_sales', 'yearly_sales'));
     }
 
     /**
